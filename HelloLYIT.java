48e77df2a38fd0622677e341965b1a80eedd331b
unknown <pmcnamara@eircom.net>
2021-02-02 15:50:57 +0000

    Added HelloLYIT.java

---
+++ HelloLYIT.java
@@ -0,0 +1,6 @@
+public class HelloLYIT {
+ public static void main(String[] args) {
+ // Prints "Hello, LYIT from Peter" as output
+ System.out.println("Hello, LYIT from Peter. A new DevOps Student");
+ }
+}
