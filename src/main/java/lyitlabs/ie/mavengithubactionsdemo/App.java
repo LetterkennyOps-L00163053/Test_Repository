df826fced80ab75f9934af9e2eecfcf1d27f985c
unknown <pmcnamara@eircom.net>
2021-03-17 00:09:41 +0000

    initial files added

---
+++ src/main/java/lyitlabs/ie/mavengithubactionsdemo/App.java
@@ -0,0 +1,13 @@
+package lyitlabs.ie.mavengithubactionsdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World and of course Donegal!" );
+    }
+}
